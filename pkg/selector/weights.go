13992d9ec1e0625388d8c98417ee1ea3b70adc8e
JP Meijers <jp.meijers@canonical.com>
2025-01-15 14:58:53 +0200

    Add more weights for future use

--- pkg/selector/weights.go
+++ pkg/selector/weights.go
@@ -1,7 +1,11 @@
 package selector
 
 var (
-	WEIGHT_GPU      int = 100
-	WEIGHT_CPU      int = 10
-	WEIGHT_CPU_FLAG int = 1
+	WEIGHT_TPU        int = 200
+	WEIGHT_TPU_VENDRO int = 20
+	WEIGHT_GPU        int = 100
+	WEIGHT_GPU_VENDOR int = 20
+	WEIGHT_CPU        int = 10
+	WEIGHT_CPU_VENDOR int = 2
+	WEIGHT_CPU_FLAG   int = 1
 )
