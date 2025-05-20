311dd8b9605aa77ae68fe101829ebbe461a4809b
JP Meijers <jp.meijers@canonical.com>
2025-05-20 14:52:46 +0200

    Add test data to test selection of intel gpu stack

--- pkg/selector/select_stack_test.go
+++ pkg/selector/select_stack_test.go
@@ -15,6 +15,7 @@ var hwInfoFiles = []string{
 	"../../test_data/hardware_info/amd-ryzen9-7900.json",
 	"../../test_data/hardware_info/dell-r730xd.json",
 	"../../test_data/hardware_info/hp-dl380p-gen8.json",
+	"../../test_data/hardware_info/mustang.json",
 	"../../test_data/hardware_info/nuc11-i5-1145G7.json",
 	"../../test_data/hardware_info/xps13-7390.json",
 	"../../test_data/hardware_info/xps13-9350.json",
@@ -296,4 +297,44 @@ func TestNoCpuInHwInfo(t *testing.T) {
 		t.Fatal("No CPU in hardware_info should return err")
 	}
 	//t.Log(err)
+}
+
+func TestCpuFlagsIntelGpu(t *testing.T) {
+	file, err := os.Open("../../test_data/hardware_info/mustang.json")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	data, err := io.ReadAll(file)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var hardwareInfo types.HwInfo
+	err = json.Unmarshal(data, &hardwareInfo)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	data, err = os.ReadFile("../../test_data/stacks/intel-gpu/stack.yaml")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var currentStack types.Stack
+	err = yaml.Unmarshal(data, &currentStack)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Valid hardware for stack
+	result, err := checkStack(hardwareInfo, currentStack)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if result == 0 {
+		t.Fatal("Stack should match")
+	}
+
 }
