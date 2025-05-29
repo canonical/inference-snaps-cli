417f2eef2b1572edd89dc7e4e4ececa9e963d976
JP Meijers <jp.meijers@canonical.com>
2025-05-29 16:02:15 +0200

    Revert moving pci struct to types pkg
    Introduce new pci peripheral struct for external use

--- pkg/types/hw_info.go
+++ pkg/types/hw_info.go
@@ -5,5 +5,5 @@ type HwInfo struct {
 	Memory         *MemoryInfo          `json:"memory,omitempty"`
 	Disk           map[string]*DirStats `json:"disk,omitempty"`
 	Gpus           []Gpu                `json:"gpus,omitempty"`
-	PciPeripherals []PciDevice          `json:"pci-peripherals,omitempty"`
+	PciPeripherals []PciPeripheral      `json:"pci-peripherals,omitempty"`
 }
