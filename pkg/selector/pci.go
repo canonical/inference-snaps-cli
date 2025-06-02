9e83b66a57959a9a593b3134eb2c41dcd66fdeac
JP Meijers <jp.meijers@canonical.com>
2025-06-02 15:56:21 +0200

    PCI add vendor and model matching

--- pkg/selector/pci.go
+++ pkg/selector/pci.go
@@ -1,24 +1,76 @@
 package selector
 
-import "github.com/canonical/ml-snap-utils/pkg/types"
+import (
+	"fmt"
+	"slices"
+	"strconv"
+
+	"github.com/canonical/ml-snap-utils/pkg/types"
+)
 
 func checkPciPeripherals(peripherals []types.PciPeripheral, device types.StackDevice) (int, error) {
 	deviceScore := 0
+
 	for _, peripheral := range peripherals {
-		/*
-			DeviceClass          string  `json:"device_class"`
-			ProgrammingInterface *string `json:"programming_interface"`
-			VendorId             string  `json:"vendor_id"`
-			DeviceId             string  `json:"device_id"`
-			SubvendorId          *string `json:"subvendor_id,omitempty"`
-			SubdeviceId          *string `json:"subdevice_id,omitempty"`
-		*/
-		if device.VendorId != nil && peripheral.VendorId != "" {
-			if *device.VendorId == peripheral.VendorId {
-				deviceScore += WeightPciVendor
+
+		vendorFound, err := checkPciVendorId(device, peripheral)
+		if err != nil {
+			return 0, err
+		}
+		if vendorFound {
+			deviceScore += WeightPciVendor
+
+			// Only check the device ID if the vendor is a match - device IDs are only unique per vendor namespace
+			modelFound, err := checkPciDeviceId(device, peripheral)
+			if err != nil {
+				return 0, err
+			}
+			if modelFound {
+				deviceScore += WeightPciModel
 			}
 		}
 	}
 
 	return deviceScore, nil
+}
+
+func checkPciVendorId(device types.StackDevice, peripheral types.PciPeripheral) (bool, error) {
+	if device.VendorId != nil && peripheral.VendorId != "" {
+		deviceVendorId, err := strconv.ParseUint(peripheral.VendorId, 0, 32)
+		if err != nil {
+			return false, fmt.Errorf("can't parse stack device vendor ID: %v", err)
+		}
+		peripheralVendorId, err := strconv.ParseUint(peripheral.VendorId, 0, 32)
+		if err != nil {
+			return false, fmt.Errorf("can't parse peripheral vendor ID: %v", err)
+		}
+		if deviceVendorId == peripheralVendorId {
+			return true, nil
+		}
+	}
+	return false, fmt.Errorf("pci device with required vendor not found")
+}
+
+func checkPciDeviceId(device types.StackDevice, peripheral types.PciPeripheral) (bool, error) {
+	// For now, we use the model-ids list in the stack manifest to match the PCI device ID
+	if len(device.ModelIds) > 0 && peripheral.DeviceId != "" {
+		var deviceModelIds []uint64
+		for _, modelIdString := range device.ModelIds {
+			modelIdInt, err := strconv.ParseUint(modelIdString, 0, 32)
+			if err != nil {
+				return false, fmt.Errorf("can't parse stack device model ID: %v", err)
+			}
+			deviceModelIds = append(deviceModelIds, modelIdInt)
+		}
+
+		peripheralModelId, err := strconv.ParseUint(peripheral.DeviceId, 0, 32)
+		if err != nil {
+			return false, fmt.Errorf("can't parse peripheral device ID: %v", err)
+		}
+
+		if slices.Contains(deviceModelIds, peripheralModelId) {
+			return true, nil
+		}
+	}
+	return false, fmt.Errorf("pci device with required device id (model id) not found")
 }
