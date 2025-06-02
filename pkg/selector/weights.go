7f491d2b97596480da2050a18ef586a789581301
JP Meijers <jp.meijers@canonical.com>
2025-06-02 15:55:28 +0200

    Fix snake case in test data
    Add negative test for NPU
    Update weights to include vendor and model

--- pkg/selector/weights.go
+++ pkg/selector/weights.go
@@ -2,15 +2,17 @@ package selector
 
 const (
 	WeightPci       = 1000
+	WeightPciModel  = 300
 	WeightPciVendor = 200
 
 	WeightGpu                  = 100
+	WeightGpuModel             = 30
 	WeightGpuVendor            = 20
 	WeightGpuVRam              = 10
 	WeightGpuComputeCapability = 10
 
 	WeightCpu       = 10
-	WeightCpuVendor = 3
-	WeightCpuModel  = 2
+	WeightCpuModel  = 3
+	WeightCpuVendor = 2
 	WeightCpuFlag   = 1
 )
