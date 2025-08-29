9b677193ad566bd17f6c92bb9e94c49fa140ebcc
JP Meijers <jp.meijers@canonical.com>
2025-08-29 10:08:12 +0200

    Add hardware info yaml annotations with kebab case

--- pkg/types/cpu.go
+++ pkg/types/cpu.go
@@ -1,14 +1,14 @@
 package types
 
 type CpuInfo struct {
-	Architecture string `json:"architecture"`
+	Architecture string `json:"architecture" yaml:"architecture"`
 
 	// amd64
-	ManufacturerId string   `json:"manufacturer_id,omitempty"`
-	Flags          []string `json:"flags,omitempty"`
+	ManufacturerId string   `json:"manufacturer_id,omitempty" yaml:"manufacturer-id,omitempty"`
+	Flags          []string `json:"flags,omitempty" yaml:"flags,omitempty"`
 
 	// arm64
-	ImplementerId HexInt   `json:"implementer_id,omitempty"`
-	PartNumber    HexInt   `json:"part_number,omitempty"`
-	Features      []string `json:"features,omitempty"`
+	ImplementerId HexInt   `json:"implementer_id,omitempty" yaml:"implementer-id,omitempty"`
+	PartNumber    HexInt   `json:"part_number,omitempty" yaml:"part-number,omitempty"`
+	Features      []string `json:"features,omitempty" yaml:"features,omitempty"`
 }
