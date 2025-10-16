be941910ab91381562380eaf445c724a564034eb
Farshid Tavakolizadeh <farshid.tavakolizadeh@canonical.com>
2025-10-16 10:43:49 +0200

    Shorten chat description

--- cmd/cli/chat.go
+++ cmd/cli/chat.go
@@ -12,7 +12,7 @@ func addChatCommand() {
 	cmd := &cobra.Command{
 		Use:               "chat",
 		Short:             "Start the chat CLI",
-		Long:              "Chat with the server via its OpenAI HTTP server.\nThis CLI supports text-based prompting only.",
+		Long:              "Chat with the server via its OpenAI API.\nThis CLI supports text-based prompting only.",
 		GroupID:           "basics",
 		Args:              cobra.NoArgs,
 		ValidArgsFunction: cobra.NoFileCompletions,
