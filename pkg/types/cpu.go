f23ccc37d34ec0c11c9d9eaf05452cce0a2f5cb3
JP Meijers <jp.meijers@canonical.com>
2025-10-21 11:47:32 +0200

    Machine info yaml/json interchangeable (#206)

--- pkg/types/cpu.go
+++ pkg/types/cpu.go
@@ -4,11 +4,11 @@ type CpuInfo struct {
 	Architecture string `json:"architecture" yaml:"architecture"`
 
 	// amd64
-	ManufacturerId string   `json:"manufacturer_id,omitempty" yaml:"manufacturer-id,omitempty"`
+	ManufacturerId string   `json:"manufacturer-id,omitempty" yaml:"manufacturer-id,omitempty"`
 	Flags          []string `json:"flags,omitempty" yaml:"flags,omitempty"`
 
 	// arm64
-	ImplementerId HexInt   `json:"implementer_id,omitempty" yaml:"implementer-id,omitempty"`
-	PartNumber    HexInt   `json:"part_number,omitempty" yaml:"part-number,omitempty"`
+	ImplementerId HexInt   `json:"implementer-id,omitempty" yaml:"implementer-id,omitempty"`
+	PartNumber    HexInt   `json:"part-number,omitempty" yaml:"part-number,omitempty"`
 	Features      []string `json:"features,omitempty" yaml:"features,omitempty"`
 }
--- pkg/types/memory.go
+++ pkg/types/memory.go
@@ -1,6 +1,6 @@
 package types
 
 type MemoryInfo struct {
-	TotalRam  uint64 `json:"total_ram" yaml:"total-ram"`
-	TotalSwap uint64 `json:"total_swap" yaml:"total-swap"`
+	TotalRam  uint64 `json:"total-ram" yaml:"total-ram"`
+	TotalSwap uint64 `json:"total-swap" yaml:"total-swap"`
 }
