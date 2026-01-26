a159c2309cca1fa5bc98b1f298185606be6bd347
Ivano Matrisciano <ivano.matrisciano@canonical.com>
2026-01-26 09:55:11 +0100

    refactor(utils): remove `FmtGigabytes`, use `FmtBytes` instead

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -21,11 +21,6 @@ func FmtPretty(v interface{}) string {
 	return string(jsonData)
 }
 
-// FmtGigabytes converts bytes to a printable string of gigabytes, rounded to the closest integer.
-func FmtGigabytes(bytes uint64) string {
-	return fmt.Sprintf("%.0fGB", float64(bytes)/1000/1000/1000)
-}
-
 // FmtBytes converts bytes to a printable string with unit
 func FmtBytes(bytes uint64) string {
 	if bytes > 1024*1024*1024*1024 {
