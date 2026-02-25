bb2b7a62e0e84b383947c22c7f223ae3b1d60e3a
Ivano Matrisciano <ivano.matrisciano@canonical.com>
2026-02-25 16:25:30 +0100

    refactor(version_test): use a table driven test with a proper name for `cleanVersionString`

--- cmd/cli/commands/version_test.go
+++ cmd/cli/commands/version_test.go
@@ -4,18 +4,35 @@ import (
 	"testing"
 )
 
-func TestVersionCleaning(t *testing.T) {
-	if cleanVersionString("") != "unset" {
-		t.Fatal("Expected empty version to be cleaned to 'unset'")
+func TestCleanVersionString(t *testing.T) {
+	tests := map[string]struct {
+		input    string
+		expected string
+	}{
+		"empty version": {
+			input:    "",
+			expected: "unset",
+		},
+		"normal version": {
+			input:    "1.2.3",
+			expected: "1.2.3",
+		},
+		"devel version": {
+			input:    "(devel)",
+			expected: "(devel)",
+		},
+		"dirty version": {
+			input:    "1.0.0+dirty",
+			expected: "1.0.0+dirty",
+		},
 	}
 
-	if cleanVersionString("1.2.3") != "1.2.3" {
-		t.Fatal("Expected non-empty version to be unchanged")
-	}
-	if cleanVersionString("(devel)") != "(devel)" {
-		t.Fatal("Expected '(devel)' version to be unchanged")
-	}
-	if cleanVersionString("1.0.0+dirty") != "1.0.0+dirty" {
-		t.Fatal("Expected dirty version to be unchanged")
+	for testName, testData := range tests {
+		t.Run(testName, func(t *testing.T) {
+			actual := cleanVersionString(testData.input)
+			if actual != testData.expected {
+				t.Errorf("cleanVersionString(%q) returned %q, expected %q", testData.input, actual, testData.expected)
+			}
+		})
 	}
 }
