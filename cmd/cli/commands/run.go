769ffa6e67ae82a50643d06f60ce13f4f3d26908
Antonio Marangi <antonio.marangi@canonical.com>
2026-04-10 12:09:02 +0200

    remove signal management

--- cmd/cli/commands/run.go
+++ cmd/cli/commands/run.go
@@ -4,8 +4,6 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
-	"os/signal"
-	"syscall"
 
 	"github.com/canonical/inference-snaps-cli/cmd/cli/common"
 	"github.com/spf13/cobra"
@@ -52,6 +50,9 @@ func (cmd *runCommand) run(_ *cobra.Command, args []string) error {
 		return fmt.Errorf("error loading engine environment: %v", err)
 	}
 
+	// this defer will not be called if this process receives a SIGTERM or a SIGKILL
+	// but it will be called if the spawned process exits no matter how
+	// signal management should be implemented to catch signals and properly call the clean() method
 	defer clean()
 
 	path := args[0]
@@ -59,21 +60,5 @@ func (cmd *runCommand) run(_ *cobra.Command, args []string) error {
 	execCmd := exec.Command(path)
 	execCmd.Stdout = os.Stdout
 	execCmd.Stderr = os.Stderr
-
-	sigChan := make(chan os.Signal, 1)
-	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM, syscall.SIGHUP, syscall.SIGQUIT, syscall.SIGUSR1, syscall.SIGUSR2)
-	defer signal.Stop(sigChan)
-
-	go func() {
-		var lastSig os.Signal
-		for sig := range sigChan {
-			if sig == lastSig {
-				fmt.Printf("Ignoring duplicate signal: %v\n", sig)
-				continue
-			}
-			lastSig = sig
-		}
-	}()
-
 	return execCmd.Run()
 }
