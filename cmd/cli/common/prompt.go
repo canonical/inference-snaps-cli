835ab92bd8c061ec4b36c73a1d37efcda6f4f32b
Farshid Tavakolizadeh <farshid.tavakolizadeh@canonical.com>
2026-04-15 10:34:28 +0200

    Default Y prompt, remove extra newline

--- cmd/cli/common/prompt.go
+++ cmd/cli/common/prompt.go
@@ -7,21 +7,52 @@ import (
 	"strings"
 )
 
-// ConfirmationPrompt prompts the user for a yes/no answer and returns true for 'y', false for 'n'.
+// ConfirmationPrompt prompts the user and returns true for 'y', false for 'n'.
+// It defaults to true if the user presses Enter without a response.
 func ConfirmationPrompt(prompt string) bool {
 	reader := bufio.NewReader(os.Stdin)
 
 	for {
-		fmt.Printf("%s [y/n] ", prompt)
-		input, _ := reader.ReadString('\n')
-		input = strings.ToLower(strings.TrimSpace(input))
+		fmt.Printf("%s [Y/n] ", prompt) // default to yes
 
-		if input == "y" || input == "yes" {
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Printf("Error reading input: %v\n", err)
+			continue
+		}
+
+		input = strings.ToLower(strings.TrimSpace(input))
+		switch input {
+		case "": // default to yes on empty input
 			return true
-		} else if input == "n" || input == "no" {
+		case "Y", "y":
+			return true
+		case "N", "n":
 			return false
-		} else {
+		default:
 			fmt.Println(`Invalid input. Please enter "y" or "n".`)
+		}
+	}
+}
+
+// ConfirmationPromptEnter prompts the user for Enter
+func ConfirmationPromptEnter(prompt string) bool {
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Printf("%s [Enter] ", prompt)
+
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Printf("Error reading input: %v\n", err)
+			continue
+		}
+		input = strings.TrimSpace(input)
+
+		if input == "" {
+			return true
+		} else {
+			fmt.Println(`Invalid input. Please press "Enter" to continue or "Ctrl+C" to cancel.`)
 		}
 	}
 }
