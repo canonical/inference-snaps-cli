988aeb74d8f6f9f0f1bc9b443038ec2f65bd0de9
Farshid Tavakolizadeh <farshid.tavakolizadeh@canonical.com>
2026-04-16 16:25:16 +0200

    Add tests for both prompts

---
+++ cmd/cli/common/prompt_test.go
@@ -0,0 +1,68 @@
+package common
+
+import (
+	"fmt"
+	"os"
+)
+
+func withStdin(input string, fn func()) {
+	originalStdin := os.Stdin
+	r, w, err := os.Pipe()
+	if err != nil {
+		panic(err)
+	}
+
+	if _, err := w.WriteString(input); err != nil {
+		panic(err)
+	}
+	_ = w.Close()
+
+	os.Stdin = r
+	defer func() {
+		os.Stdin = originalStdin
+		_ = r.Close()
+	}()
+
+	fn()
+}
+
+func printToStdout(a any) {
+	fmt.Printf("-> %v\n", a)
+}
+
+func ExamplePromptYN_defaultYes() {
+	withStdin("\n", func() {
+		printToStdout(PromptYN("Proceed?", true))
+	})
+
+	// Output:
+	// Proceed? [Y/n] -> true
+}
+
+func ExamplePromptYN_invalidThenNo() {
+	withStdin("maybe\nn\n", func() {
+		printToStdout(PromptYN("Proceed?", true))
+	})
+
+	// Output:
+	// Proceed? [Y/n] Invalid input. Please enter "y" or "n".
+	// Proceed? [Y/n] -> false
+}
+
+func ExamplePromptlnEnter() {
+	withStdin("\n", func() {
+		printToStdout(PromptlnEnter("continue"))
+	})
+
+	// Output:
+	// Press [Enter] to continue, or [q] to abort. -> true
+}
+
+func ExamplePromptlnEnter_abort() {
+	withStdin("q\n", func() {
+		printToStdout(PromptlnEnter("continue"))
+	})
+
+	// Output:
+	// Press [Enter] to continue, or [q] to abort. -> false
+}
