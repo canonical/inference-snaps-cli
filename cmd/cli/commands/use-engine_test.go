1c18740af89a8e95e05cd4fa1630e157f280c01f
Antonio Marangi <antonio.marangi@canonical.com>
2026-05-04 16:59:46 +0200

    add snapshot tests for autoselect engine

--- cmd/cli/commands/use-engine_test.go
+++ cmd/cli/commands/use-engine_test.go
@@ -49,3 +49,32 @@ func ExampleUseEngine_restartWhenEngineChanged() {
 	// Engine changed to "cpu-avx1".
 	// [mock] Restarting all services
 }
+
+func ExampleUseEngine_autoSelectEngine() {
+	cache := storage.NewMockCache()
+	config := storage.NewMockConfig()
+	cmd := useEngineCommand{
+		assumeYes: true,
+		Context: &common.Context{
+			EnginesDir: "../../../test_data/reduced-engines",
+			Cache:      cache,
+			Config:     config,
+			Snap:       snap.Mock(),
+		},
+	}
+
+	cmd.Verbose = true
+	if err := cmd.autoSelectEngine(); err != nil {
+		panic(err)
+	}
+
+	// Output:
+	// Evaluating engines for optimal hardware compatibility:
+	// ✘ ampere-altra: not compatible
+	//   - required device not found
+	// • cpu-devel: devel, score=12
+	// ✔ dummy-engine: compatible, score=132
+	// Selected engine: dummy-engine
+	// Engine changed to "dummy-engine".
+	// [mock] Restarting all services
+}
