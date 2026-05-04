1371f3e0d8e2e2510cb0769c1cc8315ebf502bb2
Ivano Matrisciano <ivano.matrisciano@canonical.com>
2026-05-04 15:48:34 +0200

    refactor(list-engines_test): extract `prepareTestData` from `TestList`

--- cmd/cli/commands/list-engines_test.go
+++ cmd/cli/commands/list-engines_test.go
@@ -1,6 +1,7 @@
 package commands
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/canonical/inference-snaps-cli/cmd/cli/common"
@@ -10,26 +11,26 @@ import (
 	"github.com/canonical/inference-snaps-cli/pkg/storage"
 )
 
-func TestList(t *testing.T) {
+func prepareTestData() (*listEnginesCommand, *outputEngines, error) {
 	cache := storage.NewMockCache()
 	err := cache.SetActiveEngine("example-memory")
 	if err != nil {
-		t.Fatalf("Error setting active engine name: %v", err)
+		return nil, nil, fmt.Errorf("Error setting active engine name: %v", err)
 	}
 
 	allEngines, err := engines.LoadManifests("../../../test_data/engines")
 	if err != nil {
-		t.Fatalf("error loading engines: %v", err)
+		return nil, nil, fmt.Errorf("error loading engines: %v", err)
 	}
 
 	hardwareInfo, err := hardware_info.GetFromRawData("xps13-7390", true, "../../../test_data")
 	if err != nil {
-		t.Fatalf("error getting hardware info: %v", err)
+		return nil, nil, fmt.Errorf("error getting hardware info: %v", err)
 	}
 
 	scoredEngines, err := selector.ScoreEngines(hardwareInfo, allEngines)
 	if err != nil {
-		t.Fatalf("error scoring engines: %v", err)
+		return nil, nil, fmt.Errorf("error scoring engines: %v", err)
 	}
 
 	// cmd.printEnginesTable needs to call `cmd.Cache.GetActiveEngine()` to get the current active engine
@@ -43,7 +44,7 @@ func TestList(t *testing.T) {
 
 	activeEngine, err := cmd.Cache.GetActiveEngine()
 	if err != nil {
-		t.Fatalf("%s: %v", common.LookingUpActiveEngine, err)
+		return nil, nil, fmt.Errorf("%s: %v", common.LookingUpActiveEngine, err)
 	}
 
 	enginesList := outputEngines{
@@ -54,12 +55,21 @@ func TestList(t *testing.T) {
 		enginesList.Engines = append(enginesList.Engines, common.NewEngineDetails(se))
 	}
 
-	err = cmd.printEnginesJson(enginesList)
+	return &cmd, &enginesList, nil
+}
+
+func TestList(t *testing.T) {
+	cmd, enginesList, err := prepareTestData()
+	if err != nil {
+		t.Fatalf("Error preparing test data: %v", err)
+	}
+
+	err = cmd.printEnginesJson(*enginesList)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	err = cmd.printEnginesTable(enginesList)
+	err = cmd.printEnginesTable(*enginesList)
 	if err != nil {
 		t.Fatal(err)
 	}
