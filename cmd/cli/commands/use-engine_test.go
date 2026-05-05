1e7ca8f5336888e4e74cd3dbbfabb6713b1608ab
Antonio Marangi <antonio.marangi@canonical.com>
2026-05-05 10:25:07 +0200

    robust snapshot test

--- cmd/cli/commands/use-engine_test.go
+++ cmd/cli/commands/use-engine_test.go
@@ -2,6 +2,9 @@ package commands
 
 import (
 	"github.com/canonical/inference-snaps-cli/cmd/cli/common"
+	"github.com/canonical/inference-snaps-cli/pkg/engines"
+	"github.com/canonical/inference-snaps-cli/pkg/hardware_info"
+	"github.com/canonical/inference-snaps-cli/pkg/selector"
 	"github.com/canonical/inference-snaps-cli/pkg/snap"
 	"github.com/canonical/inference-snaps-cli/pkg/storage"
 )
@@ -56,25 +59,48 @@ func ExampleUseEngine_autoSelectEngine() {
 	cmd := useEngineCommand{
 		assumeYes: true,
 		Context: &common.Context{
-			EnginesDir: "../../../test_data/reduced-engines",
+			EnginesDir: "../../../test_data/engines",
 			Cache:      cache,
 			Config:     config,
 			Snap:       snap.Mock(),
 		},
 	}
-
+	cmd.Cache.SetActiveEngine("")
 	cmd.Verbose = true
-	if err := cmd.autoSelectEngine(); err != nil {
+	notCompatibleManifest, err := engines.LoadManifest(cmd.Context.EnginesDir, "not-compatible-engine")
+	if err != nil {
+		panic(err)
+	}
+	cpuDevelManifest, err := engines.LoadManifest(cmd.Context.EnginesDir, "cpu-devel")
+	if err != nil {
+		panic(err)
+	}
+	cpuManifest, err := engines.LoadManifest(cmd.Context.EnginesDir, "cpu")
+	if err != nil {
+		panic(err)
+	}
+
+	allEngines := []engines.Manifest{*notCompatibleManifest, *cpuDevelManifest, *cpuManifest}
+	machineInfo, err := hardware_info.GetFromRawData("mustang", true, "../../../test_data")
+	if err != nil {
+		panic(err)
+	}
+
+	scoredEngines, err := selector.ScoreEngines(machineInfo, allEngines)
+	if err != nil {
+		panic(err)
+	}
+	if err := cmd.autoSelectEngineWithScoredEngines(scoredEngines); err != nil {
 		panic(err)
 	}
 
 	// Output:
 	// Evaluating engines for optimal hardware compatibility:
-	// ✘ ampere-altra: not compatible
+	// ✘ not-compatible-engine: not compatible
 	//   - required device not found
 	// • cpu-devel: devel, score=12
-	// ✔ dummy-engine: compatible, score=12
-	// Selected engine: dummy-engine
-	// Engine changed to "dummy-engine".
+	// ✔ cpu: compatible, score=12
+	// Selected engine: cpu
+	// Engine changed to "cpu".
 	// [mock] Restarting all services
 }
