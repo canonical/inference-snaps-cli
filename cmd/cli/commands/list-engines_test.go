e5773cebd823aaaf4c85923be703b5402b317abc
Ivano Matrisciano <ivano.matrisciano@canonical.com>
2026-05-06 12:42:15 +0200

    test(list-engines_test): update table snapshot

--- cmd/cli/commands/list-engines_test.go
+++ cmd/cli/commands/list-engines_test.go
@@ -97,21 +97,23 @@ func TestGetEnginesTable(t *testing.T) {
 		t.Fatalf("Error getting engines table: %v", err)
 	}
 
-	expectedTable := `ENGINE           VENDOR             DESCRIPTION                          COMPAT
-intel-cpu        Intel Corporation  Use Intel CPUs                       yes   
-cpu-avx2         Canonical Ltd      CPUs with AVX2                       yes   
-cpu-avx1         Canonical Ltd      Legacy CPUs with only SSE4.2 (2008…  yes   
-cpu-devel        Canonical Ltd      Requires any CPU but is grade devel  devel 
-intel-gpu        Intel Corporation  Modern Intel GPUs (>=gen 13)         no    
-example-memory*  Canonical Ltd      Legacy CPUs, offering full accurac…  no    
-cuda-generic     Canonical Ltd      Nvidia GPUs using CUDA. All major …  no    
-rocm-generic     Canonical Ltd      AMD GPUs using ROCm. All major ROC…  no    
-arm-neon         Canonical Ltd      ARM CPUs with NEON instruction set   no    
-ampere-altra     Canonical Ltd      Test ampere selection                no    
-ampere           Canonical Ltd      Test ampere selection                no    
-intel-npu        Intel Corporation  Intel NPUs                           no    
-amd-gpu          Canonical Ltd      AMD specific engine targeting only…  no    
-cpu-avx512       Canonical Ltd      CPUs with AVX512                     no    
+	expectedTable := `ENGINE                 VENDOR             DESCRIPTION                    COMPAT
+intel-cpu              Intel Corporation  Use Intel CPUs                 yes   
+cpu-avx2               Canonical Ltd      CPUs with AVX2                 yes   
+cpu-avx1               Canonical Ltd      Legacy CPUs with only SSE4.2…  yes   
+cpu                    Canonical Ltd      General CPU engine             yes   
+cpu-devel              Canonical Ltd      Requires any CPU but is grad…  devel 
+rocm-generic           Canonical Ltd      AMD GPUs using ROCm. All maj…  no    
+not-compatible-engine  Canonical Ltd      This test engine is designed…  no    
+intel-npu              Intel Corporation  Intel NPUs                     no    
+intel-gpu              Intel Corporation  Modern Intel GPUs (>=gen 13)   no    
+ampere                 Canonical Ltd      Test ampere selection          no    
+example-memory*        Canonical Ltd      Legacy CPUs, offering full a…  no    
+cuda-generic           Canonical Ltd      Nvidia GPUs using CUDA. All …  no    
+amd-gpu                Canonical Ltd      AMD specific engine targetin…  no    
+cpu-avx512             Canonical Ltd      CPUs with AVX512               no    
+ampere-altra           Canonical Ltd      Test ampere selection          no    
+arm-neon               Canonical Ltd      ARM CPUs with NEON instructi…  no    
 `
 
 	if tableStr != expectedTable {
