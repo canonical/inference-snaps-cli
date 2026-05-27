881d184cae2fb72210da4e1595038714469ba395
Antonio Marangi <antonio.marangi@canonical.com>
2026-05-27 17:58:52 +0200

    save work

--- cmd/cli/commands/list-engines_test.go
+++ cmd/cli/commands/list-engines_test.go
@@ -102,7 +102,7 @@ intel-cpu              Intel Corporation  Use Intel CPUs                 yes
 cpu-avx2               Canonical Ltd      CPUs with AVX2                 yes   
 cpu-avx1               Canonical Ltd      Legacy CPUs with only SSE4.2…  yes   
 cpu                    Canonical Ltd      General CPU engine             yes   
-cpu-devel              Canonical Ltd      Requires any CPU but is grad…  devel 
+cpu-devel              Canonical Ltd      Requires any CPU but is grad…  exptl 
 rocm-generic           Canonical Ltd      AMD GPUs using ROCm. All maj…  no    
 not-compatible-engine  Canonical Ltd      This test engine is designed…  no    
 intel-npu              Intel Corporation  Intel NPUs                     no    
