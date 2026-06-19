d89814b12c1ada10e23bdbe97a5170e2168d6b39
Ivano Matrisciano <ivano.matrisciano@canonical.com>
2026-06-19 16:27:50 +0200

    Apply suggestion: add usage instructions to snap completer script
    skip-checks: true

--- completer/snap.bash
+++ completer/snap.bash
@@ -1,4 +1,25 @@
-# Bash completion for the app named after the snap
+# Bash completion script for modelctl packaged in a snap
+#
+# To use, in snapcraft.yaml:
+# 1. Set apps.<app-name>.completer to the name of this script for the app.
+# 2. Add a symlink from <app> to modelctl binary.
+#
+# For example, snapcraft.yaml:
+# name: mymodel
+# <...>
+# parts:
+#   cli:
+#     source: <...>
+#     plugin: dump
+#     override-build: |
+#       # For tab completion
+#       ln --symbolic ./modelctl bin/mymodel
+#       craftctl default
+#     apps:
+#       mymodel: # command named after the snap
+#         command: bin/modelctl
+#         completer: bin/snap-completer.bash
+#
 
 # Unset the _init_completion function from the bash-completion package to force
 # use of the basic but functional internal implementation.
