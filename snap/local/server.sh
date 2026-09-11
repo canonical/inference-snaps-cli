22fbc3a87aa48e09ab2df6157fb5a1ee5870ff1d
Farshid Tavakolizadeh <farshid.tavakolizadeh@canonical.com>
2026-09-11 17:47:19 +0200

    Disable python buffer to get mock server logs

--- snap/local/server.sh
+++ snap/local/server.sh
@@ -6,4 +6,5 @@ port="$(modelctl get http.port)"
 host="$(modelctl get http.host)"
 
 echo "Starting mock OpenAI server on $host:$port"
+export PYTHONUNBUFFERED=1
 exec modelctl run -- "$SNAP"/bin/mock-openai-server/server.py --port "$port" --host "$host" --reasoning --delay 0.05
